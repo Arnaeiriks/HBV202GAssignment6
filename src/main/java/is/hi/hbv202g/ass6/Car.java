1c52044195c1d0065ea3b0c9445917cdd2ef7bc6
Arnaeiriks <are45@hi.is>
2023-02-21 10:35:42 +0000

    refactor

--- src/main/java/is/hi/hbv202g/ass6/Car.java
+++ src/main/java/is/hi/hbv202g/ass6/Car.java
@@ -1,9 +1,13 @@
 package is.hi.hbv202g.ass6;
 
 class Car {
-    String name;
+    static String name;
 
     Car(String name) {
         this.name = name;
+    }
+
+    public static String getNameOfCar() {
+        return name;
     }
 }
